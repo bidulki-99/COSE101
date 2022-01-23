e12ef23f929c141c0bd9e106748dd046abb4f2e6
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-01-24 02:23:50 +0900

    Create hand_output.c

---
+++ hand_output.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void fun1(void);
+void fun2(void);
+void fun3(void);
+
+int x = 2;
+int main(void)
+{
+	int x = 3;
+	printf("%d, ", x);
+	{
+		int x = 5;
+		printf("%d, ", x);
+	}
+	printf("%d, ", x);
+
+	fun1(); fun2(); fun3();
+	fun1(); fun2(); fun3();
+	printf("%d\n", x);
+}
+
+void fun1(void)
+{
+	int x = 7;
+	printf("%d, ", x);
+	x++;
+	printf("%d, ", x);
+}
+
+void fun2(void)
+{
+	static int x = 11;
+	printf("%d, ", x);
+	x++;
+	printf("%d, ", x);
+}
+
+void fun3(void)
+{
+	printf("%d, ", x);
+	x += 13;
+	printf("%d, ", x);
+}
