3deb52d7a229d9294178b2612ed0acb1b592a28e
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-04-29 19:37:37 +0900

    Create MID3_5.c

---
+++ 2022/MID3_5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    char pw[100] = {0};
+    int check[8] = {0, 1, 0, 0, 0, 0, 0, 1};
+    scanf("%s", pw);
+
+    int length = 0;
+    while (pw[length])
+        length++;
+
+    if (9 <= length && length <= 20)
+        check[2]++;
+
+    for (int i = 0; i < length; i++) {
+        if ('a' <= pw[i] && pw[i] <= 'z')
+            check[3]++;
+        else if ('A' <= pw[i] && pw[i] <= 'Z')
+            check[4]++;
+        else if ('0' <= pw[i] && pw[i] <= '9')
+            check[5]++;
+        else if (pw[i] == '!' || pw[i] == '@' || pw[i] == '#')
+            check[6]++;
+        else
+            check[1]--;
+        
+        if (i >= 2 && pw[i] == pw[i-1] && pw[i-1] == pw[i-2])
+            check[7]--;
+    }
+
+    int correct = 1;
+
+    for (int i = 1; i <= 7; i++) {
+        if (check[i] == 0) {
+            if (i == 3 && check[i] < 2)
+                printf("3");
+            else
+                printf("%d", i);
+
+            correct = 0;
+        }
+    }
+
+    if (correct == 1)
+        printf("0");    
+}
